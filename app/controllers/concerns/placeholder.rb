dff608e3b39576f892cb1b6d6a501a242563a5c2
Teresa <teresa_klausecker@yahoo.de>
2020-04-03 16:46:05 +0200

    Updated file upload forms and refactored placeholder functionality into view

--- app/controllers/concerns/placeholder.rb
+++
@@ -1,7 +0,0 @@
-module Placeholder
-  extend ActiveSupport::Concern
-
-  def self.image_generator(height:, width:, placeholder_color: "000")
-    "https://via.placeholder.com/#{height}x#{width}/#{placeholder_color}"
-  end
-end
