b214370856bd61d5f081ca04e449f81059e6eb80
Teresa <teresa_klausecker@yahoo.de>
2020-04-06 18:07:56 +0200

    Implemented initial connection to Twitter API

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
+    config.eager_load_paths << "#{Rails.root}/lib"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
