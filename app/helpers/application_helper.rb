01b5883f7105f377988ccd192cb592c326feb3f3
Teresa <teresa_klausecker@yahoo.de>
2020-04-06 14:59:32 +0200

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -93,4 +93,16 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+  
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Achtung Portfolio!!", sticky: false, time: 10000)
+  end
+
 end
